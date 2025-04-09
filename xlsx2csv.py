be435f795d2a4840320e32bca5f179d9ce72d6ef
richard-ma <richard.ma.19850509@gmail.com>
2025-04-09 23:08:30 +0800

    添加xls和xlsx到csv的转换器

---
+++ xlsx2csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# 读取Excel文件并转换为DataFrame对象
+df1 = pd.read_excel('1.xlsx')
+df2 = pd.read_excel('2.xlsx')
+
+# 将DataFrame对象写入CSV文件
+df1.to_csv('1.csv', index=False)
+df2.to_csv('2.csv', index=False)
